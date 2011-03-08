ae45c95d6d5a66d72d390688fed44607728b6b6c
Mathias Ertl <mati@fsinf.at>
2011-03-08 16:48:32 +0100

    add initial version of a main script

---
+++ iptables-fsinf.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+INCLUDEDIR='include/'
+ETCDIR='.'
+CONFDIR='.'
+CONFINCLUDEDIR="$CONFDIR/conf.d"
+INITDIR="$CONFDIR/init.d"
+
+# include functions
+for file in $(find $INCLUDEDIR -type f -or -type l | sort | grep '.sh$')
+do
+	. $file
+done
+
+for file in $(find $CONFINCLUDEDIR -type f -or -type l | sort | grep '/[0-9][0-9]')
+do
+	. $file
+done
+
+# initialize:
+init
+
+# execute additional rules the system has in $INITDIR
+echo
+echo "# Add rules found in $INITDIR"
+for file in $(find $INITDIR -type f -or -type l | sort | grep '/[0-9][0-9]')
+do
+	. $file
+done
+echo "# Finished adding rules found in $INITDIR"
+echo 
+
+# COUNT, ICMP and BLOCKED chains are inserted last, but on top of our
+# filter-rules.
+
+# allow only some icmp
+icmp
+
+# block some hosts:
+block_hosts
+
+# count traffic
+count_traffic
+
